0214c8344e2a5ce671488de8ee4d8f8434636dd0
Berend Wouters <berend.wouters@gmail.com>
2019-05-23 19:59:34 +0200

    feat(auth) adds authGuard to the fetchDataComponent
    AB#40

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './components/auth/auth.component';
+import { AuthGuard } from './core/services/auth.guard';
 
 const routes: Routes = [
    { path: '', component: HomeComponent, pathMatch: 'full' },
    { path: 'counter', component: CounterComponent },
-   { path: 'fetch-data', component: FetchDataComponent },
+   { path: 'fetch-data', component: FetchDataComponent, canActivate:[AuthGuard] },
    { path: 'login', component: AuthComponent },
 ];
 
