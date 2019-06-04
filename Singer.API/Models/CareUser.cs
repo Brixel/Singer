334562ec98ee8dc1da7cbc17ddb7b0e09b86ef04
wim <van.laer.wim@gmail.com>
2019-06-04 21:30:07 +0200

    feat(care-users) Format CareUsers.cs
    AB#12

--- Singer.API/Models/CareUser.cs
+++ Singer.API/Models/CareUser.cs
@@ -7,18 +7,25 @@ namespace Singer.Models
    {
       [PersonalData]
       public DateTime BirthDay { get; set; }
+
       [PersonalData]
       public string CaseNumber { get; set; }
+
       [PersonalData]
       public AgeGroup AgeGroup { get; set; }
+
       [PersonalData]
       public bool IsExtern { get; set; }
+
       [PersonalData]
       public bool HasTrajectory { get; set; }
+
       [PersonalData]
       public bool HasNormalDayCare { get; set; }
+
       [PersonalData]
       public bool HasVacationDayCare { get; set; }
+
       [PersonalData]
       public bool HasResources { get; set; }
    }
