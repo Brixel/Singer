f5beaa9dafdd64b088166b77c4ebd9aa10169b15
wim <van.laer.wim@gmail.com>
2019-06-04 23:16:15 +0200

    feat(careusers) Created care user mock service
    AB#12

--- Singer.API/Services/MockCareUserService.cs
+++ Singer.API/Services/MockCareUserService.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
-using Singer.Data.Models;
 using Singer.DTOs;
 using Singer.Services.Interfaces;
 
@@ -8,32 +10,129 @@ namespace Singer.Services
 {
    public class MockCareUserService : ICareUsersService
    {
+      #region FIELDS
+
+      private readonly IList<CareUserDTO> _mockData = new List<CareUserDTO>
+      {
+         new CareUserDTO
+         {
+            Name = "Joske Vermeulen",
+            BirthDay = DateTime.Parse("06/07/2008", CultureInfo.InvariantCulture),
+            CaseNumber = "0123456789",
+            AgeGroup = Models.AgeGroup.Child,
+            IsExtern = false,
+            HasTrajectory = true,
+            HasNormalDayCare = true,
+            HasVacationDayCare = true,
+            HasResources = true
+         },
+         new CareUserDTO
+         {
+            Name = "Kim Janssens",
+            BirthDay = DateTime.Parse("08/07/2006", CultureInfo.InvariantCulture),
+            CaseNumber = "9876543210",
+            AgeGroup = Models.AgeGroup.Child,
+            IsExtern = true,
+            HasTrajectory = true,
+            HasNormalDayCare = true,
+            HasVacationDayCare = true,
+            HasResources = true
+         },
+         new CareUserDTO
+         {
+            Name = "Benjamin Vermeulen",
+            BirthDay = DateTime.Parse("06/08/2010", CultureInfo.InvariantCulture),
+            CaseNumber = "091837465",
+            AgeGroup = Models.AgeGroup.Youngster,
+            IsExtern = false,
+            HasTrajectory = true,
+            HasNormalDayCare = true,
+            HasVacationDayCare = true,
+            HasResources = false
+         },
+      };
+
+      #endregion FIELDS
+
       #region METHODS
 
-      public Task<CareUserDTO> CreateCareUser(CareUserDTO careUser)
+      public async Task<CareUserDTO> CreateCareUserAsync(CareUserDTO careUser)
       {
-         throw new System.NotImplementedException();
+         // generate new id
+         careUser.Id = Guid.NewGuid().ToString();
+         // add the new care user
+         _mockData.Add(careUser);
+         // return the new created care user
+         return await Task.FromResult(careUser);
       }
 
-      public Task<PaginationModel<CareUserDTO>> GetAllCareUsers()
+      public async Task<IList<CareUserDTO>> GetAllCareUsersAsync()
       {
-         throw new System.NotImplementedException();
+         // return all the mock data
+         return await Task.FromResult(_mockData);
       }
 
-      public Task<CareUserDTO> GetCareUser(string id)
+      public async Task<CareUserDTO> GetCareUserAsync(string id)
       {
-         throw new System.NotImplementedException();
+         // check valid id
+         Guid.Parse(id);
+         // return the care user with the given id
+         return await Task.FromResult(_mockData.Single(x => x.Id == id));
       }
 
-      public Task<CareUserDTO> UpdateCareUser(CareUserDTO careUser, string id,
-         IEnumerable<string> propertiesToUpdate = null)
+      public async Task<CareUserDTO> UpdateCareUserAsync(
+         CareUserDTO careUser,
+         string id,
+         IList<string> propertiesToUpdate = null)
       {
-         throw new System.NotImplementedException();
+         // check valid id
+         Guid.Parse(id);
+         // get the index of the care user with the given id
+         var i = _mockData
+            .Select((user, index) => new {User = user, Index = index})
+            .Single(x => x.User.Id == id)
+            .Index;
+
+         // set the care user's id to the given id 
+         careUser.Id = id;
+         if (propertiesToUpdate == null)
+         {
+            // update the complete care user
+            _mockData[i] = careUser;
+            // return the updated care user
+            return await Task.FromResult(_mockData[i]);
+         }
+
+         // get the properties of the CareUserDTO without the id property
+         var props = typeof(CareUserDTO)
+            .GetProperties()
+            .Where(x => x.Name != nameof(CareUserDTO.Id));
+
+         // update all the properties listed in the properties to update
+         foreach (var propertyInfo in props)
+         {
+            if (propertiesToUpdate.Any(x => x == propertyInfo.Name))
+               propertyInfo.SetValue(_mockData[i], propertyInfo.GetValue(careUser));
+         }
+
+         // return the new care user
+         return await Task.FromResult(_mockData[i]);
       }
 
-      public Task DeleteCareUser(string id)
+      public async Task DeleteCareUserAsync(string id)
       {
-         throw new System.NotImplementedException();
+         // check valid id
+         Guid.Parse(id);
+         // get the index of the care user with the given id
+         var i = _mockData
+            .Select((user, index) => new { User = user, Index = index })
+            .Single(x => x.User.Id == id)
+            .Index;
+
+         // remove the care user from the mock list
+         _mockData.RemoveAt(i);
+
+         await Task.CompletedTask;
       }
 
       #endregion METHODS
