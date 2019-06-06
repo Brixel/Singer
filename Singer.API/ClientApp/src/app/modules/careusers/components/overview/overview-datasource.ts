99447d2a3462082c2bf6bc5bbcebf7ecf088cc88
joren <joren.thijs@student.ucll.be>
2019-06-07 01:21:10 +0200

    feat(careusers) overview-datasource.ts cleanup
    AB#12

--- Singer.API/ClientApp/src/app/modules/careusers/components/overview/overview-datasource.ts
+++ Singer.API/ClientApp/src/app/modules/careusers/components/overview/overview-datasource.ts
@@ -1,7 +1,4 @@
-import { DataSource } from '@angular/cdk/collections';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { map, takeUntil } from 'rxjs/operators';
-import { Observable, of as observableOf, merge } from 'rxjs';
 import { CareUsersAPIService } from 'src/app/modules/core/services/care-users-api/care-users-api.service';
 
 // Care User class
@@ -87,115 +84,9 @@ export class OverviewDataSource extends MatTableDataSource<CareUser> {
       this.data = careUsersAPI.fetchCareUsersData();
    }
 
-   //#region old connect method
-   // Had to be removed when switching from "export class OverviewDataSource extends DataSource<CareUser>" to
-   //  "export class OverviewDataSource extends MatTableDataSource<CareUser>"
-   // Switched to "export class OverviewDataSource extends MatTableDataSource<CareUser>" to enable filtering
-
-   /**
-    * Connect this data source to the table. The table will only update when
-    * the returned stream emits new items.
-    * @returns A stream of the items to be rendered.
-    */
-   // connect(): Observable<CareUser[]> {
-   //    // Combine everything that affects the rendered data into one update
-   //    // stream for the data-table to consume.
-   //    const dataMutations = [
-   //       observableOf(this.data),
-   //       this.paginator.page,
-   //       this.sort.sortChange,
-   //    ];
-
-   //    // Set the paginator's length
-   //    this.paginator.length = this.data.length;
-
-   //    return merge(...dataMutations).pipe(
-   //       map(() => {
-   //          return this.getPagedData(this.getSortedData([...this.data]));
-   //       })
-   //    );
-   // }
-   //#endregion
-
    /**
     *  Called when the table is being destroyed. Use this function, to clean up
     * any open connections or free any held resources that were set up during connect.
     */
    disconnect() {}
-
-   //#region old paginator and sorting methods
-
-   // was no longer used after switching from "export class OverviewDataSource extends DataSource<CareUser>" to
-   // "export class OverviewDataSource extends MatTableDataSource<CareUser>" and removing the connect method
-   // Switched to "export class OverviewDataSource extends MatTableDataSource<CareUser>" to enable filtering
-
-   /**
-    * Paginate the data (client-side). If you're using server-side pagination,
-    * this would be replaced by requesting the appropriate data from the server.
-    */
-   // private getPagedData(data: CareUser[]) {
-   //    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-   //    return data.splice(startIndex, this.paginator.pageSize);
-   // }
-
-   /**
-    * Sort the data (client-side). If you're using server-side sorting,
-    * this would be replaced by requesting the appropriate data from the server.
-    */
-   // private getSortedData(data: CareUser[]) {
-   //    if (!this.sort.active || this.sort.direction === '') {
-   //       return data;
-   //    }
-
-   //    return data.sort((a, b) => {
-   //       const isAsc = this.sort.direction === 'asc';
-   //       switch (this.sort.active) {
-   //          case 'id':
-   //             return compare(a.id, b.id, isAsc);
-   //          case 'firstName':
-   //             return compare(a.firstName, b.firstName, isAsc);
-   //          case 'lastName':
-   //             return compare(a.lastName, b.lastName, isAsc);
-   //          case 'email':
-   //             return compare(a.email, b.email, isAsc);
-   //          case 'userName':
-   //             return compare(a.userName, b.userName, isAsc);
-   //          case 'birthday':
-   //             return compare(a.birthday, b.birthday, isAsc);
-   //          case 'caseNumber':
-   //             return compare(a.caseNumber, b.caseNumber, isAsc);
-   //          case 'ageGroup':
-   //             return compare(a.ageGroup, b.ageGroup, isAsc);
-   //          case 'isExtern':
-   //             return compare(a.isExtern, b.isExtern, isAsc);
-   //          case 'hasTrajectory':
-   //             return compare(a.hasTrajectory, b.hasTrajectory, isAsc);
-   //          case 'hasNormalDayCare':
-   //             return compare(a.hasNormalDayCare, b.hasNormalDayCare, isAsc);
-   //          case 'hasVacationDayCare':
-   //             return compare(
-   //                a.hasVacationDayCare,
-   //                b.hasVacationDayCare,
-   //                isAsc
-   //             );
-   //          case 'hasResources':
-   //             return compare(a.hasResources, b.hasResources, isAsc);
-   //          default:
-   //             return 0;
-   //       }
-   //    });
-   // }
-   //#endregion
 }
-
-//#region old sorting method
-
-   // was no longer used after switching from "export class OverviewDataSource extends DataSource<CareUser>" to
-   // "export class OverviewDataSource extends MatTableDataSource<CareUser>" and removing the connect method
-   // Switched to "export class OverviewDataSource extends MatTableDataSource<CareUser>" to enable filtering
-
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-// function compare(a, b, isAsc) {
-//    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-// }
-//#endregion
