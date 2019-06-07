490c24bb1e30f3dd8b314e5f815462f8df2998dc
wim <van.laer.wim@gmail.com>
2019-06-07 22:08:20 +0200

    feat(careusers) Created user DTO and madeCareUserDTO inherit from it
    AB#12

--- Singer.API/Services/Interfaces/IUserInterface.cs
+++ Singer.API/Services/Interfaces/IUserInterface.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Singer.Data.Models;
 using Singer.DTOs;
+using Singer.Models;
 
 namespace Singer.Services.Interfaces
 {
    public interface ICareUserService
    {
-      Task<CareUserDTO> CreateCareUserAsync(CareUserDTO careUser);
+      Task<UserDTO> CreateCareUserAsync(CareUserDTO careUser);
       Task<IList<CareUserDTO>> GetAllCareUsersAsync();
       Task<CareUserDTO> GetCareUserAsync(string id);
       Task<CareUserDTO> UpdateCareUserAsync(CareUserDTO careUser, string id, IList<string> propertiesToUpdate = null);
