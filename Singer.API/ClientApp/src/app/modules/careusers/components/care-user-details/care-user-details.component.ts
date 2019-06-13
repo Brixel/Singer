4f7538a17d6d22f2e759bb742ccadb4c243535a6
joren <joren.thijs@student.ucll.be>
2019-06-13 16:28:10 +0200

    feat(careusers)
    changed inputfields from value binding
    to formcontrol binding
    
    AB#14

--- Singer.API/ClientApp/src/app/modules/careusers/components/care-user-details/care-user-details.component.ts
+++ Singer.API/ClientApp/src/app/modules/careusers/components/care-user-details/care-user-details.component.ts
@@ -25,21 +25,6 @@ export class CareUserDetailsComponent implements OnInit {
 
    //#region Binding properties for form:
 
-   // Form Values
-   idFieldValue: string;
-   firstNameFieldValue: string;
-   lastNameFieldValue: string;
-   emailFieldValue: string;
-   userNameFieldValue: string;
-   birthdayFieldValue: Date;
-   caseNumberFieldValue: string;
-   ageGroupFieldValue: string;
-   isExternFieldValue: string;
-   hasTrajectoryFieldValue: string;
-   hasNormalDayCareFieldValue: string;
-   hasVacationDayCareFieldValue: string;
-   hasResourcesFieldValue: string;
-
    // From placeholders
    idFieldPlaceholder: string = 'ID';
    firstNameFieldPlaceholder: string = 'Voornaam';
@@ -60,13 +45,42 @@ export class CareUserDetailsComponent implements OnInit {
    birthdayDatePickerMaxDate = new Date();
 
    // Form controls
+   idFieldControl = new FormControl('', [Validators.required]);
+   firstNameFieldControl = new FormControl('', [Validators.required]);
+   lastNameFieldControl = new FormControl('', [Validators.required]);
    emailFieldControl = new FormControl('', [
       Validators.required,
       Validators.email,
    ]);
+   userNameFieldControl = new FormControl('', [Validators.required]);
+   birthdayFieldControl = new FormControl('', [Validators.required]);
    caseNumberFieldControl = new FormControl('', [Validators.required]);
+   ageGroupFieldControl = new FormControl('', [Validators.required]);
+   isExternFieldControl = new FormControl('', [Validators.required]);
+   hasTrajectoryFieldControl = new FormControl('', [Validators.required]);
+   hasNormalDayCareFieldControl = new FormControl('', [Validators.required]);
+   hasVacationDayCareFieldControl = new FormControl('', [Validators.required]);
+   hasResourcesFieldControl = new FormControl('', [Validators.required]);
 
    // Error messages for required fields
+   getIdFieldErrorMessage() {
+      return this.idFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getFirstNameFieldErrorMessage() {
+      return this.firstNameFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getLastNameFieldErrorMessage() {
+      return this.lastNameFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
    getEmailFieldErrorMessage() {
       return this.emailFieldControl.hasError('required')
          ? 'You must enter a value'
@@ -74,8 +88,58 @@ export class CareUserDetailsComponent implements OnInit {
          ? 'Not a valid email'
          : '';
    }
+
+   getUserNameFieldErrorMessage() {
+      return this.userNameFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getBirthdayErrorMessage() {
+      return this.birthdayFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+
    getCaseNumberFieldErrorMessage() {
       return this.caseNumberFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getAgeGroupFieldErrorMessage() {
+      return this.ageGroupFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getIsExternFieldErrorMessage() {
+      return this.isExternFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getHasTrajectoryFieldErrorMessage() {
+      return this.hasTrajectoryFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getHasNormalDayCareFieldErrorMessage() {
+      return this.hasNormalDayCareFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getHasVacationDayCareFieldErrorMessage() {
+      return this.hasVacationDayCareFieldControl.hasError('required')
+         ? 'You must enter a value'
+         : '';
+   }
+
+   getHasResourcesFieldErrorMessage() {
+      return this.hasResourcesFieldControl.hasError('required')
          ? 'You must enter a value'
          : '';
    }
@@ -93,106 +157,99 @@ export class CareUserDetailsComponent implements OnInit {
    }
 
    ngOnInit() {
-      this.isAdding ? this.clearFormValues() : this.updateFormValues();
+      this.isAdding ? this.resetFormControls() : this.updateFormValues();
    }
 
    private updateFormValues() {
-      this.idFieldValue = this.currentCareUserInstance.id;
-      this.firstNameFieldValue = this.currentCareUserInstance.firstName;
-      this.lastNameFieldValue = this.currentCareUserInstance.lastName;
-      this.emailFieldValue = this.currentCareUserInstance.email;
-      this.userNameFieldValue = this.currentCareUserInstance.userName;
-      this.birthdayFieldValue = this.currentCareUserInstance.birthday;
-      this.caseNumberFieldValue = this.currentCareUserInstance.caseNumber;
-      this.ageGroupFieldValue =
+      this.idFieldControl.setValue(this.currentCareUserInstance.id);
+      this.firstNameFieldControl.setValue(this.currentCareUserInstance.firstName);
+      this.lastNameFieldControl.setValue(this.currentCareUserInstance.lastName);
+      this.emailFieldControl.setValue(this.currentCareUserInstance.email);
+      this.userNameFieldControl.setValue(this.currentCareUserInstance.userName);
+      this.birthdayFieldControl.setValue(this.currentCareUserInstance.birthday);
+      this.caseNumberFieldControl.setValue(this.currentCareUserInstance.caseNumber);
+      this.ageGroupFieldControl.setValue(
          this.currentCareUserInstance.ageGroup === 'Kinderen'
             ? 'child'
-            : 'youngster';
-      this.isExternFieldValue = this.currentCareUserInstance.isExtern
+            : 'youngster');
+      this.isExternFieldControl.setValue(this.currentCareUserInstance.isExtern
          ? 'true'
-         : 'false';
-      this.hasTrajectoryFieldValue = this.currentCareUserInstance.hasTrajectory
+         : 'false');
+      this.hasTrajectoryFieldControl.setValue(this.currentCareUserInstance.hasTrajectory
          ? 'true'
-         : 'false';
-      this.hasNormalDayCareFieldValue = this.currentCareUserInstance
+         : 'false');
+      this.hasNormalDayCareFieldControl.setValue(this.currentCareUserInstance
          .hasNormalDayCare
          ? 'true'
-         : 'false';
-      this.hasVacationDayCareFieldValue = this.currentCareUserInstance
+         : 'false');
+      this.hasVacationDayCareFieldControl.setValue(this.currentCareUserInstance
          .hasVacationDayCare
          ? 'true'
-         : 'false';
-      this.hasResourcesFieldValue = this.currentCareUserInstance.hasResources
+         : 'false');
+      this.hasResourcesFieldControl.setValue(this.currentCareUserInstance.hasResources
          ? 'true'
-         : 'false';
-   }
-
-   private clearFormValues() {
-      // Clear all form fields
-      this.idFieldValue = '';
-      this.firstNameFieldValue = '';
-      this.lastNameFieldValue = '';
-      this.emailFieldValue = '';
-      this.birthdayFieldValue = null;
-      this.caseNumberFieldValue = '';
-      this.ageGroupFieldValue = '';
-      this.isExternFieldValue = '';
-      this.hasTrajectoryFieldValue = '';
-      this.hasNormalDayCareFieldValue = '';
-      this.hasVacationDayCareFieldValue = '';
-      this.hasResourcesFieldValue = '';
-      //Reset Form Controls
-      this.resetFormControls();
+         : 'false');
    }
 
    private resetFormControls() {
+      // Clear all form fields
+      this.idFieldControl.reset();
+      this.firstNameFieldControl.reset();
+      this.lastNameFieldControl.reset();
       this.emailFieldControl.reset();
+      this.birthdayFieldControl.reset();
       this.caseNumberFieldControl.reset();
+      this.ageGroupFieldControl.reset();
+      this.isExternFieldControl.reset();
+      this.hasTrajectoryFieldControl.reset();
+      this.hasNormalDayCareFieldControl.reset();
+      this.hasVacationDayCareFieldControl.reset();
+      this.hasResourcesFieldControl.reset();
    }
 
    checkForChanges(): boolean {
       debugger;
       console.log(this.caseNumberFieldControl.value);
       //If we are editing an existing user and there are no changes return false
-      if (this.currentCareUserInstance.id !== this.idFieldValue) return true;
-      if (this.currentCareUserInstance.firstName !== this.firstNameFieldValue)
+      if (this.currentCareUserInstance.id !== this.idFieldControl.value) return true;
+      if (this.currentCareUserInstance.firstName !== this.firstNameFieldControl.value)
          return true;
-      if (this.currentCareUserInstance.lastName !== this.lastNameFieldValue)
+      if (this.currentCareUserInstance.lastName !== this.lastNameFieldControl.value)
          return true;
-      if (this.currentCareUserInstance.email !== this.emailFieldValue)
+      if (this.currentCareUserInstance.email !== this.emailFieldControl.value)
          return true;
-      if (this.currentCareUserInstance.birthday !== this.birthdayFieldValue)
+      if (this.currentCareUserInstance.birthday !== this.birthdayFieldControl.value)
          return true;
-      if (this.currentCareUserInstance.caseNumber !== this.caseNumberFieldValue)
+      if (this.currentCareUserInstance.caseNumber !== this.caseNumberFieldControl.value)
          return true;
       if (
          this.currentCareUserInstance.ageGroup !==
-         (this.ageGroupFieldValue === 'child' ? 'Kinderen' : 'Jongeren')
+         (this.ageGroupFieldControl.value === 'child' ? 'Kinderen' : 'Jongeren')
       )
          return true;
       if (
          this.currentCareUserInstance.isExtern !==
-         (this.isExternFieldValue === 'true' ? true : false)
+         (this.isExternFieldControl.value === 'true' ? true : false)
       )
          return true;
       if (
          this.currentCareUserInstance.hasTrajectory !==
-         (this.hasTrajectoryFieldValue === 'true' ? true : false)
+         (this.hasTrajectoryFieldControl.value === 'true' ? true : false)
       )
          return true;
       if (
          this.currentCareUserInstance.hasNormalDayCare !==
-         (this.hasNormalDayCareFieldValue === 'true' ? true : false)
+         (this.hasNormalDayCareFieldControl.value === 'true' ? true : false)
       )
          return true;
       if (
          this.currentCareUserInstance.hasVacationDayCare !==
-         (this.hasVacationDayCareFieldValue === 'true' ? true : false)
+         (this.hasVacationDayCareFieldControl.value === 'true' ? true : false)
       )
          return true;
       if (
          this.currentCareUserInstance.hasResources !==
-         (this.hasResourcesFieldValue === 'true' ? true : false)
+         (this.hasResourcesFieldControl.value === 'true' ? true : false)
       )
          return true;
       return false;
@@ -207,21 +264,21 @@ export class CareUserDetailsComponent implements OnInit {
 
    private updateCurrentCareUserInstance() {
       this.currentCareUserInstance = {
-         id: this.idFieldValue,
-         firstName: this.firstNameFieldValue,
-         lastName: this.lastNameFieldValue,
-         email: this.emailFieldValue,
+         id: this.idFieldControl.value,
+         firstName: this.firstNameFieldControl.value,
+         lastName: this.lastNameFieldControl.value,
+         email: this.emailFieldControl.value,
          userName: '',
-         birthday: this.birthdayFieldValue,
-         caseNumber: this.caseNumberFieldValue,
-         ageGroup: this.ageGroupFieldValue,
-         isExtern: this.isExternFieldValue === 'true' ? true : false,
-         hasTrajectory: this.hasTrajectoryFieldValue === 'true' ? true : false,
+         birthday: this.birthdayFieldControl.value,
+         caseNumber: this.caseNumberFieldControl.value,
+         ageGroup: this.ageGroupFieldControl.value,
+         isExtern: this.isExternFieldControl.value === 'true' ? true : false,
+         hasTrajectory: this.hasTrajectoryFieldControl.value === 'true' ? true : false,
          hasNormalDayCare:
-            this.hasNormalDayCareFieldValue === 'true' ? true : false,
+            this.hasNormalDayCareFieldControl.value === 'true' ? true : false,
          hasVacationDayCare:
-            this.hasVacationDayCareFieldValue === 'true' ? true : false,
-         hasResources: this.hasResourcesFieldValue === 'true' ? true : false,
+            this.hasVacationDayCareFieldControl.value === 'true' ? true : false,
+         hasResources: this.hasResourcesFieldControl.value === 'true' ? true : false,
       };
    }
 
