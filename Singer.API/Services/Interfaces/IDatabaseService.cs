b51f0049698273b3bcc046fe03708638e5a1561e
wim <van.laer.wim@gmail.com>
2019-06-20 21:35:15 +0200

    refactor(service-abstraction): created database service inteface

---
+++ Singer.API/Services/Interfaces/IDatabaseService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Singer.Helpers;
+
+namespace Singer.Services.Interfaces
+{
+   public interface IService<TEntity, TDTO>
+      where TEntity : IIdentifiable
+   {
+      Task<TDTO> CreateAsync(TDTO dto);
+
+      Task<TDTO> GetOneAsync(Guid id);
+      Task<IReadOnlyList<TDTO>> GetAllAsync();
+      Task<TDTO> GetAsync(string sortColumn,
+         string sortDirection,
+         string filter,
+         int page = 0,
+         int userPerPage = 15);
+
+      Task<TDTO> UpdateAsync(TDTO newValue, Guid id);
+
+      Task DeleteAsync(Guid id);
+   }
+}
