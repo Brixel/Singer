c06ec98c48ba21e5e7ec10dc54b0937d7a7de1a7
Berend Wouters <berend.wouters@gmail.com>
2019-06-25 21:58:08 +0200

    chore fix display to dutch

--- Singer.API/DTOs/CreateUserDTO.cs
+++ Singer.API/DTOs/CreateUserDTO.cs
@@ -10,18 +10,18 @@ namespace Singer.DTOs
       [StringLength(maximumLength: 255,
          ErrorMessage = "De {0} moet een lengte hebben van minstens {2} en maximum {1} karakters.",
          MinimumLength = 3)]
-      [DisplayName("FirstName")]
+      [DisplayName("Voornaam")]
       public string FirstName { get; set; }
       [Required]
       [StringLength(maximumLength: 255,
          ErrorMessage = "De {0} moet een lengte hebben van minstens {2} en maximum {1} karakters.",
          MinimumLength = 3)]
-      [DisplayName("LastName")]
+      [DisplayName("Achternaam")]
       public string LastName { get; set; }
 
       [Required]
       [EmailAddress]
-      [DisplayName("E-mail address")]
+      [DisplayName("E-mail adres")]
       public string Email { get; set; }
 
       [Required]
