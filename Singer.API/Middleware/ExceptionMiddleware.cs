754a4943ad919eb0a14588c4e3f0ecb2985269a3
wim <van.laer.wim@gmail.com>
2019-06-25 21:32:38 +0200

    refactor(service-abstraction): added documentation

--- Singer.API/Middleware/ExceptionMiddleware.cs
+++ Singer.API/Middleware/ExceptionMiddleware.cs
@@ -7,16 +7,33 @@ using Singer.Helpers.Exceptions;
 
 namespace Singer.Middleware
 {
+   /// <summary>
+   /// Middle ware that catches all exceptions that thrown.
+   /// </summary>
    public class ExceptionMiddleware
    {
+      /// <summary>
+      /// The next delegate to be invoked.
+      /// </summary>
       private readonly RequestDelegate _next;
+
+      /// <summary>
+      /// The hosting environment. (Is used to check whether the environment is in development)
+      /// </summary>
       private readonly IHostingEnvironment _env;
 
+
+      /// <summary>
+      /// Constructs a new instance of the <see cref="ExceptionMiddleware"/>
+      /// </summary>
+      /// <param name="next">The next delegate to be invoked.</param>
+      /// <param name="env">The hosting environment. (Is used to check whether the environment is in development)</param>
       public ExceptionMiddleware(RequestDelegate next, IHostingEnvironment env)
       {
          _next = next;
          _env = env;
       }
+
 
       public async Task InvokeAsync(HttpContext context)
       {
