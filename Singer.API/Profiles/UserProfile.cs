56419be65397682bc1683161b117bcbac7062287
Alex Goris <alex.goris@fastlikehell.com>
2019-07-18 19:41:46 +0200

    feat(LegalGuardianUsers): Added  map from LegalGuardianDTO to LegalGuardian

--- Singer.API/Profiles/UserProfile.cs
+++ Singer.API/Profiles/UserProfile.cs
@@ -11,11 +11,12 @@ namespace Singer.Profiles
       public UserProfile()
       {
          CreateMap<User, CareUserDTO>();
-         CreateMap<CareUser, CareUserDTO>().ForMember(x=>x.Id, opt => opt.MapFrom(src => src.Id));
+         CreateMap<CareUser, CareUserDTO>().ForMember(x => x.Id, opt => opt.MapFrom(src => src.Id));
          CreateMap<CareUserDTO, CareUser>().ForMember(
             x => x.Id, opt => opt.MapFrom(src => src.Id.ToString())
          );
          CreateMap<CreateCareUserDTO, CareUser>();
+         CreateMap<LegalGuardianUserDTO, LegalGuardianUser>();
       }
    }
 }
