95f72b66e6e67b340ff23ead3aa479088148394c
vyruz1986 <alex.goris@fastlikehell.com>
2019-07-20 20:37:43 +0200

    fix(UserService): Fix deleting of User
    Also added protection against invalid id in GetOneAsync

--- Singer.API/Services/UserService.cs
+++ Singer.API/Services/UserService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
@@ -9,6 +10,8 @@ using Microsoft.EntityFrameworkCore;
 using Singer.Data;
 using Singer.DTOs.Users;
 using Singer.Helpers.Exceptions;
+using Singer.Helpers.Extensions;
+using Singer.Models;
 using Singer.Models.Users;
 
 namespace Singer.Services
@@ -58,12 +61,51 @@ namespace Singer.Services
             projector = EntityToDTOProjector;
 
          // search for the entity with the given id in the database
-         var item = await DbSet.Include(x => x.User).Select(projector).Where(x => x.Id == id).SingleAsync();
+         var item = await DbSet.Include(x => x.User).Select(projector).Where(x => x.Id == id).SingleOrDefaultAsync();
          if (item == null)
             throw new NotFoundException();
 
          // return the found entity projected to it's DTO
          return item;
+      }
+
+      public override async Task DeleteAsync(Guid id)
+      {
+         // search for the entity to delete
+         var itemToDelete = await DbSet.FindAsync(id);
+         if (itemToDelete == null)
+            throw new NotFoundException();
+
+         //Since users are entities based on the `User` entity, we must delete the User isntead of the specific entity
+         var userToDelete = await Context.Users.FindAsync(itemToDelete.Id);
+         Context.Users.Remove(userToDelete);
+         DbSet.Remove(itemToDelete);
+         await Context.SaveChangesAsync();
+      }
+
+      public override async Task<SearchResults<TUserDTO>> GetAsync(
+         string filter = null,
+         Expression<Func<TUserEntity, TUserDTO>> projector = null,
+         Expression<Func<TUserDTO, object>> orderer = null,
+         ListSortDirection sortDirection = ListSortDirection.Ascending,
+         int page = 0,
+         int entitiesPerPage = 15)
+      {
+         // set the projector if it is null
+         if (projector == null)
+            projector = EntityToDTOProjector;
+
+         // return the paged results
+         return await DbSet
+            .Include(x => x.User)
+            .AsQueryable()
+            .ToPagedListAsync(
+               filterExpression: Filter(filter),
+               projectionExpression: projector,
+               orderByLambda: orderer,
+               sortDirection: sortDirection,
+               pageIndex: page,
+               pageSize: entitiesPerPage);
       }
 
    }
