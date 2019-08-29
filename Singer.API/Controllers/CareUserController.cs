8985027329405b504b6312e0652b43ec78232721
Alex Goris <alex.goris@fastlikehell.com>
2019-08-29 21:50:45 +0200

    feat(CareUsers): Implemented linking of LG USers to CareUsers
    You can now pass AddLegalGuardianUsers and RemoveLegalGuardianUsers to
    the update careuser method

--- Singer.API/Controllers/CareUserController.cs
+++ Singer.API/Controllers/CareUserController.cs
@@ -8,6 +8,7 @@ using Singer.DTOs.Users;
 using Singer.DTOs;
 using Singer.Models.Users;
 using Singer.Services;
+using Singer.Helpers.Exceptions;
 
 namespace Singer.Controllers
 {
@@ -19,6 +20,31 @@ namespace Singer.Controllers
       public CareUserController(CareUserService databaseService) : base(databaseService)
       {
          careUserService = databaseService;
+      }
+
+      [HttpPut("{id}")]
+      [ProducesResponseType(StatusCodes.Status200OK)]
+      [ProducesResponseType(StatusCodes.Status404NotFound)]
+      [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+      public override async Task<ActionResult> Update(Guid id, [FromBody]UpdateCareUserDTO dto)
+      {
+         if (dto is null)
+         {
+            throw new BadInputException(nameof(dto));
+         }
+
+         if ((dto.LegalGuardianUsersToAdd?.Count ?? 0) > 0)
+         {
+            await careUserService.AddLinkedUsers(id, dto.LegalGuardianUsersToAdd);
+         }
+
+         if ((dto.LegalGuardianUsersToRemove?.Count ?? 0) > 0)
+         {
+            await careUserService.RemoveLinkedUsers(id, dto.LegalGuardianUsersToRemove);
+         }
+
+         var result = await DatabaseService.UpdateAsync(id, dto);
+         return Ok(result);
       }
    }
 }
--- Singer.API/Services/CareUserService.cs
+++ Singer.API/Services/CareUserService.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Singer.Data;
 using Singer.DTOs.Users;
+using Singer.Helpers.Exceptions;
 using Singer.Models.Users;
 
 namespace Singer.Services
@@ -32,6 +35,62 @@ namespace Singer.Services
                f.User.LastName.Contains(filter) ||
                f.CaseNumber.Contains(filter);
          return filterExpression;
+      }
+
+
+      public async Task AddLinkedUsers(Guid CareUserId, List<Guid> NewLinkedUsers)
+      {
+         // First check if LGUser exists
+         var CareUser = await Context.CareUsers.FindAsync(CareUserId);
+         if (CareUser == null)
+         {
+            throw new BadInputException($"Tried to add user link for non existing CareUser with id {CareUserId}");
+         }
+
+         List<LegalGuardianCareUser> NewCareUsers = new List<LegalGuardianCareUser>();
+         foreach (var u in NewLinkedUsers)
+         {
+            var LegalGuardianUser = await Context.LegalGuardianUsers.FirstOrDefaultAsync(c => c.Id == u);
+            if (LegalGuardianUser == null)
+            {
+               throw new BadInputException($"Tried to link LG User which does not exist (id={u})");
+            }
+            var LinkedUserExists = await Context.LegalGuardianCareUsers.FirstOrDefaultAsync(
+               x => x.LegalGuardianId == u
+               && x.CareUserId == CareUserId
+            );
+            if (LinkedUserExists != null)
+            {
+               throw new BadInputException($"Duplicate link was attempted to add: LGUserId: {u}, CareUserID: {CareUserId}");
+            }
+            NewCareUsers.Add(new LegalGuardianCareUser() { CareUserId = CareUserId, LegalGuardianId = u });
+         }
+         CareUser.LegalGuardianCareUsers = NewCareUsers;
+         await Context.SaveChangesAsync();
+      }
+
+      public async Task RemoveLinkedUsers(Guid CareUserId, List<Guid> UsersToRemove)
+      {
+         // First check if LGUser exists
+         var CareUser = await Context.CareUsers.FindAsync(CareUserId);
+         if (CareUser == null)
+         {
+            throw new BadInputException($"Tried to remove user link for non existing CareUser with id {CareUserId}");
+         }
+
+         foreach (var u in UsersToRemove)
+         {
+            var LinkedUserExists = await Context.LegalGuardianCareUsers.FirstOrDefaultAsync(
+               x => x.LegalGuardianId == u
+               && x.CareUserId == CareUserId
+            );
+            if (LinkedUserExists == null)
+            {
+               throw new BadInputException($"You tried to remove a care user from a CareUser which was not linked to begin with (LG ID: {u})");
+            }
+            CareUser.LegalGuardianCareUsers.Remove(LinkedUserExists);
+         }
+         await Context.SaveChangesAsync();
       }
    }
 }
