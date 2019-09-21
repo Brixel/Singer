a9374a046fcbf93c35221d977e0151af1b32fc58
Berend Wouters <berend.wouters@gmail.com>
2019-09-21 23:11:10 +0200

    bug set pageindex to 0 instead of 1 on sortchange

--- Singer.API/ClientApp/src/app/modules/admin/components/legalguardians/legalguardian-overview/legalguardian-overview.component.ts
+++ Singer.API/ClientApp/src/app/modules/admin/components/legalguardians/legalguardian-overview/legalguardian-overview.component.ts
@@ -102,12 +102,12 @@ export class LegalguardianOverviewComponent implements OnInit, AfterViewInit {
             debounceTime(400),
             distinctUntilChanged(),
             tap(() => {
-               this.paginator.pageIndex = 1;
+               this.paginator.pageIndex = 0;
                this.loadLegalGuardians();
             })
          )
          .subscribe();
-      this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 1));
+      this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
       merge(this.sort.sortChange, this.paginator.page)
          .pipe(
             tap(() => {
