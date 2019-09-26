3372eb212de71449d6d4c3916f5851be71120c1f
wim van laer <van.laer.wim@gmail.com>
2019-09-26 21:58:48 +0200

    feat/add-user-to-event: implemented create on event service
    AB#76

--- Singer.API/Services/EventService.cs
+++ Singer.API/Services/EventService.cs
@@ -10,20 +10,53 @@ using Singer.DTOs;
 using Singer.Models;
 using Singer.Services.Interfaces;
 using Singer.Profiles;
-using Microsoft.EntityFrameworkCore.Internal;
+using Singer.Helpers.Extensions;
 
 namespace Singer.Services
 {
-   public class EventService : DatabaseService<Event, EventDTO, CreateEventDTO, UpdateEventDTO>,
-      IEventService
+   public class EventService
+      : DatabaseService<Event, EventDTO, CreateEventDTO, UpdateEventDTO>, IEventService
    {
       public EventService(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
       {
       }
 
+
       protected override DbSet<Event> DbSet => Context.Events;
 
       protected override IQueryable<Event> Queryable => Context.Events.Include(x => x.Location);
+
+
+      public override async Task<EventDTO> CreateAsync(CreateEventDTO dto)
+      {
+         var start = dto.StartDate;
+         var end = dto.EndDate;
+
+         // add slots for all the days in the event
+         var slots = new List<EventSlot>();
+         var diff = end - start;
+         for (var i = 0; i <= diff.Days; i++)
+         {
+            var date = start + TimeSpan.FromDays(i);
+            slots.Add(new EventSlot
+            {
+               StartDateTime = date,
+               EndDateTime = date.SetTime(end),
+            });
+         }
+
+         // project the DTO to an entity
+         var entity = Mapper.Map<Event>(dto);
+         entity.EventSlots = slots;
+
+         Context.Add(entity);
+         await Context.SaveChangesAsync();
+
+         var returnEntity = await GetOneAsync(entity.Id);
+
+         // return the new created entity
+         return returnEntity;
+      }
 
       protected override Expression<Func<Event, bool>> Filter(string filter)
       {
