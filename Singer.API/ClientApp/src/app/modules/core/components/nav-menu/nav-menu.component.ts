257435b82eaaeda5864662c5b09216b72e151272
Joren Thijs <joren.thijs@hotmail.com>
2019-10-26 00:06:11 +0200

    feat(navbar-restyling) restyled navbar
    removed old nav toolbar
    added new generative code to generate mat tab nav
    added new routerlink requirement methods
    added new extra menu in top right corner
    added background animation to menu button
    added icons to menu options
    added new styling

--- Singer.API/ClientApp/src/app/modules/core/components/nav-menu/nav-menu.component.ts
+++ Singer.API/ClientApp/src/app/modules/core/components/nav-menu/nav-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs';
+import { SingerRouterLink, singerRouterLinkRequirements } from '../../models/singer-routerlink.model';
+import { AdminAuthGuard } from '../../services/admin-auth.guard';
 
 @Component({
    selector: 'app-nav-menu',
@@ -12,6 +14,64 @@ export class NavMenuComponent {
 
    @Output() logoutEvent = new EventEmitter();
    isAdmin: boolean;
+
+   routerLinks:SingerRouterLink[] = [
+      {
+         RouterLinkName: "Start",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.none,
+         ],
+         routerLink: '/dashboard',
+      },
+      {
+         RouterLinkName: "Voogden",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.isAdmin,
+            singerRouterLinkRequirements.isAuthenticated
+         ],
+         routerLink:'/admin/voogden',
+      },
+      {
+         RouterLinkName: "Zorg gebruikers",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.isAdmin,
+            singerRouterLinkRequirements.isAuthenticated
+         ],
+         routerLink:'/admin/zorggebruikers',
+      },
+      {
+         RouterLinkName: "Beheerders",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.isAdmin,
+            singerRouterLinkRequirements.isAuthenticated
+         ],
+         routerLink:'/admin/beheerders',
+      },
+      {
+         RouterLinkName: "Evenementen",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.isAdmin,
+            singerRouterLinkRequirements.isAuthenticated
+         ],
+         routerLink:'/admin/evenementen',
+      },
+      {
+         RouterLinkName: "Login",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.isNotAuthenticated
+         ],
+         routerLink:'/login',
+      },
+   ]
+
+   activeRouterLink:SingerRouterLink = this.routerLinks[0];
+   emptyRouterLink:SingerRouterLink = {
+      RouterLinkName: "",
+         RouterLinkRequirements: [
+            singerRouterLinkRequirements.none
+         ],
+         routerLink:'/',
+   }
 
    constructor(private authService: AuthService) {
       this.authService.isAdmin$.subscribe((res) => {
@@ -32,5 +92,30 @@ export class NavMenuComponent {
 
    toggle() {
       this.isExpanded = !this.isExpanded;
+   }
+
+   // Checks a list of RouterLinkRequirements
+   checkRequirements(requirements:singerRouterLinkRequirements[]):boolean {
+
+      // Expect all requirements to be met
+      var result:boolean = true;
+
+      // Loop through the requirements
+      requirements.forEach(requirement => {
+         // If a requirement is not met: result = false
+         if(!this.checkRequirement(requirement)) result = false;
+      });
+
+      // If all requirements are checked: return result
+      return result;
+   }
+
+   // Checks an individual RouterLinkRequirement
+   checkRequirement(requirement:string):boolean {
+         debugger;
+         if(requirement == singerRouterLinkRequirements.none) return true;
+         if(requirement == singerRouterLinkRequirements.isAdmin) return this.isAdmin;
+         if(requirement == singerRouterLinkRequirements.isAuthenticated) return this.isAuthenticated();
+         if(requirement == singerRouterLinkRequirements.isNotAuthenticated) return !this.isAuthenticated();
    }
 }
---
+++ Singer.API/ClientApp/src/app/modules/core/models/singer-routerlink.model.ts
@@ -0,0 +1,14 @@
+import { Route } from "@angular/router";
+
+export interface SingerRouterLink {
+   RouterLinkName:string,
+   RouterLinkRequirements:singerRouterLinkRequirements[],
+   routerLink:string,
+}
+
+export enum singerRouterLinkRequirements {
+   none = "none",
+   isAuthenticated= "isAuthenticated",
+   isAdmin = "isAdmin",
+   isNotAuthenticated = "isNotAuthenticated",
+}
