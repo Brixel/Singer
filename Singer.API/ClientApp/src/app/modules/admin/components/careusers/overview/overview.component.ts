8a8c2415b11372d4fb8bddf1a525dd456326e18f
Berend Wouters <berend.wouters@gmail.com>
2019-11-08 12:59:11 +0100

    fix(daycare-location-update)

--- Singer.API/ClientApp/src/app/modules/admin/components/careusers/overview/overview.component.ts
+++ Singer.API/ClientApp/src/app/modules/admin/components/careusers/overview/overview.component.ts
@@ -67,6 +67,7 @@ export class OverviewComponent implements OnInit, AfterViewInit {
       });
 
       dialogRef.componentInstance.submitEvent.subscribe((result: CareUser) => {
+
          // Update the Careuser
          this.careUserService.updateUser(result).subscribe(res => {
             // Reload Careusers
