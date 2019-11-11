2a101a17c1e3b9c82689f1837af2fc3334c65a60
Berend Wouters <berend.wouters@gmail.com>
2019-11-11 22:07:37 +0100

    feat(registrations) view registrations per day
    AB#28

--- Singer.API/ClientApp/src/app/modules/admin/components/singerevents/singer-event-registrations/singer-event-registrations.component.ts
+++ Singer.API/ClientApp/src/app/modules/admin/components/singerevents/singer-event-registrations/singer-event-registrations.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { SingerEvent, RegistrationStatus } from 'src/app/modules/core/models/singerevent.model';
+import { SingerEvent } from 'src/app/modules/core/models/singerevent.model';
 import { FormGroup } from '@angular/forms';
-import { CareUser, CareUserDTO } from 'src/app/modules/core/models/careuser.model';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { SingerEventsService } from 'src/app/modules/core/services/singerevents-api/singerevents.service';
-import { Registrant } from 'src/app/modules/core/models/registrant.model';
+import { Registrant, EventSlot } from 'src/app/modules/core/models/registrant.model';
+import { RegistrationStatus } from 'src/app/modules/core/models/enum';
 
 export class SingerEventRegistrationData {
    event: SingerEvent;
@@ -17,9 +17,11 @@ export class SingerEventRegistrationData {
    styleUrls: ['./singer-event-registrations.component.css'],
 })
 export class SingerEventRegistrationsComponent implements OnInit {
-   registrants: Registrant[] = [];
    formGroup: FormGroup;
    event: SingerEvent;
+   eventSlots: EventSlot[];
+   selectedEventSlot: EventSlot;
+   registrationStatus = RegistrationStatus;
 
    constructor(
       private singerEventService: SingerEventsService,
@@ -31,24 +33,23 @@ export class SingerEventRegistrationsComponent implements OnInit {
    }
 
    ngOnInit() {
-      this.singerEventService.getEventRegistrations(this.event.id, 'asc', 'startDateTime', 0, 1000, '').subscribe(res => console.log(res));
+      this.singerEventService
+         .getEventRegistrations(this.event.id, 'asc', 'startDateTime', 0, 1000, '')
+         .subscribe(res => {
+            this.eventSlots = res.map(r => new EventSlot(r.id, r.startDateTime, r.endDateTime, r.registrations));
+
+            const currentDate = Date.now();
+            const nextEventSlots =
+               this.eventSlots
+                  .filter(a => a.startDateTime.getTime() >= currentDate)
+                  .sort((a, b) => a.startDateTime.getTime() - b.startDateTime.getTime());
+            this.selectedEventSlot = nextEventSlots.length > 0 ? nextEventSlots[0] : null;
+         });
    }
 
-   private createRegistrants(registrants: Registrant[]) {
-      const uniqueNames = registrants.filter(function(elem, index, self) {
-         // TODO Improve this once we need to approve multiple eventslots separate
-
-         return index === self.map(c => c.careUserId).indexOf(elem.careUserId);
-      });
-      return uniqueNames;
-   }
 
    close() {
       this.dialogRef.close();
-   }
-
-   save() {
-      const registrantIds = this.registrants.map(r => r.careUserId);
    }
 }
 
--- Singer.API/ClientApp/src/app/modules/core/models/registrant.model.ts
+++ Singer.API/ClientApp/src/app/modules/core/models/registrant.model.ts
@@ -1,13 +1,50 @@
-import { RegistrationStatus, EventRelevantCareUserDTO } from 'src/app/modules/core/models/singerevent.model';
+import { RegistrationStatus, EventRelevantCareUserDTO, EventCareUserRegistrationDTO } from 'src/app/modules/core/models/singerevent.model';
 import { CareUserDTO } from 'src/app/modules/core/models/careuser.model';
 export class Registrant {
    careUserId: string;
    name: string;
    registrationStatus: RegistrationStatus;
 
-   constructor(careUserId: string, firstName: string, lastName: string, registrationStatus: RegistrationStatus){
+   constructor(careUserId: string, firstName: string, lastName: string, registrationStatus: RegistrationStatus) {
       this.careUserId = careUserId;
       this.name = `${firstName} ${lastName}`;
       this.registrationStatus = registrationStatus;
+   }
+}
+
+
+
+export class EventSlot {
+   id: string;
+   startDateTime: Date;
+   endDateTime: Date;
+   registrants: Registrant[];
+   constructor(id: string, startDateTime: Date, endDateTime: Date, registrations: EventCareUserRegistrationDTO[]) {
+      this.id = id;
+      this.startDateTime = new Date(startDateTime);
+      this.endDateTime = new Date(endDateTime);
+      this.registrants =
+         registrations.map(reg => new Registrant(reg.careUserId, reg.firstName, reg.lastName, reg.status))
+         .sort(this.sorter());
+   }
+
+   private sorter(): (a: Registrant, b: Registrant) => number {
+      return (a, b) => {
+         // First sort by status
+         if (a.registrationStatus < b.registrationStatus) {
+            return -1;
+         } else if (a.registrationStatus > b.registrationStatus) {
+            return 1;
+         }
+
+         // then Sort by name
+         if (a.name < b.name) {
+            return -1;
+         } else if (a.name > b.name) {
+            return 1;
+         } else {
+            return 0;
+         }
+      };
    }
 }
