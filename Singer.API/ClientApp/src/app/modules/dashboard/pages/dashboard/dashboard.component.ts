81e70bdd904a83f1ae0eed3a971105aba8992f9f
Joren Thijs <joren.thijs@hotmail.com>
2019-11-12 13:15:49 +0100

    feat(family-registration-wizard) added authentication service.
    AB#57

--- Singer.API/ClientApp/src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ Singer.API/ClientApp/src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { SingerEvent, EventDescription } from 'src/app/modules/core/models/singerevent.model';
+import {
+   SingerEvent,
+   EventDescription,
+} from 'src/app/modules/core/models/singerevent.model';
 import { AgeGroup } from 'src/app/modules/core/models/enum';
+import { AuthService } from 'src/app/modules/core/services/auth.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+   selector: 'app-dashboard',
+   templateUrl: './dashboard.component.html',
+   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() {
-  }
+   isAuthenticated = false;
 
+   constructor(private authService: AuthService) {}
+
+   ngOnInit() {
+      this.authService.isAuthenticated$.subscribe((res) => {
+         this.isAuthenticated = res;
+      });
+
+      this.authService.isAuthenticated();
+   }
 }
