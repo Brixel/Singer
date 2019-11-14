b01ab65f272e4a98ada3c0b32b5cef298d37ef05
Joren Thijs <joren.thijs@hotmail.com>
2019-11-14 19:47:31 +0100

    feat(wizard) separated dashboard components
    AB#57

--- Singer.API/ClientApp/src/app/modules/dashboard/components/shared-components/event-list/event-list.component.ts
+++ Singer.API/ClientApp/src/app/modules/dashboard/components/shared-components/event-list/event-list.component.ts
@@ -1,15 +1,15 @@
 import {
    Component,
    OnInit,
-   Input,
-   ViewChild,
-   Output,
-   EventEmitter,
 } from '@angular/core';
-import { EventDescription } from 'src/app/modules/core/models/singerevent.model';
-import { MatDrawer } from '@angular/material';
+import { EventDescription, SearchEventDTO } from 'src/app/modules/core/models/singerevent.model';
 import { SearchEventData } from '../event-search/event-search.component';
 import { SingerEventLocation } from 'src/app/modules/core/models/singer-event-location';
+import { ApiService } from 'src/app/modules/core/services/api.service';
+import { Observable } from 'rxjs';
+import { PaginationDTO } from 'src/app/modules/core/models/pagination.model';
+import { HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
    selector: 'app-event-list',
@@ -17,27 +17,81 @@ import { SingerEventLocation } from 'src/app/modules/core/models/singer-event-lo
    styleUrls: ['./event-list.component.css'],
 })
 export class EventListComponent implements OnInit {
-   @Input() events: EventDescription[];
-   @Input() availableLocations: SingerEventLocation[];
 
-   @Output() searchEvent: EventEmitter<SearchEventData> = new EventEmitter();
-
-   @ViewChild('drawer') drawer: MatDrawer;
    breakpoint: number;
 
-   constructor() {}
+   events: EventDescription[] = [];
+   availableLocations: SingerEventLocation[];
+
+   constructor(private apiService: ApiService) {}
 
    ngOnInit() {
       this.breakpoint = window.innerWidth <= 500 ? 1 : 3;
-      const searchEvent = <SearchEventData>{};
-      this.searchEvent.emit(searchEvent);
+
+      this.getSingerEventLocations('asc', 'name', 0, 1000, '').subscribe(
+         res => {
+            this.availableLocations = res.items as SingerEventLocation[];
+         }
+      );
+
+      // Make first searchevent to load all events
+      var emptySearchEventData:SearchEventData = {
+         startDateTime: null,
+         endDateTime: null,
+         locationId: '',
+
+      };
+
+      this.onSearchEvent(emptySearchEventData);
+   }
+
+   getSingerEventLocations(
+      sortDirection?: string,
+      sortColumn?: string,
+      pageIndex?: number,
+      pageSize?: number,
+      filter?: string
+   ): Observable<PaginationDTO> {
+      const searchParams = new HttpParams()
+         .set('sortDirection', sortDirection)
+         .set('sortColumn', sortColumn)
+         .set('pageIndex', pageIndex.toString())
+         .set('pageSize', pageSize.toString())
+         .set('filter', filter);
+      return this.apiService
+         .get('api/eventlocation', searchParams)
+         .pipe(map(res => res));
    }
 
    onResize(event) {
       this.breakpoint = event.target.innerWidth <= 500 ? 1 : 3;
    }
 
-   onSearchEvent(searchEvent: SearchEventData) {
-      this.searchEvent.emit(searchEvent);
+   getEvents(searchEventData: SearchEventData): Observable<EventDescription[]> {
+      const searchParams = <SearchEventDTO>{
+         startDate: searchEventData.startDateTime,
+         endDate: searchEventData.endDateTime,
+         locationId: searchEventData.locationId,
+      };
+      return this.apiService
+         .post('api/event/search', searchParams)
+         .pipe(map(res => res));
+   }
+
+   onSearchEvent(searchEventData: SearchEventData) {
+      this.getEvents(searchEventData).subscribe(
+         res =>
+            (this.events = res.map(
+               r =>
+                  new EventDescription(
+                     r.id,
+                     r.title,
+                     r.description,
+                     r.ageGroups,
+                     r.startDateTime,
+                     r.endDateTime
+                  )
+            ))
+      );
    }
 }
--- Singer.API/ClientApp/src/app/modules/dashboard/components/shared-components/home/home.component.ts
+++ Singer.API/ClientApp/src/app/modules/dashboard/components/shared-components/home/home.component.ts
@@ -18,67 +18,11 @@ import { AuthService } from 'src/app/modules/core/services/auth.service';
    styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-   isAuthenticated = false;
-   events: EventDescription[] = [];
-   availableLocations: SingerEventLocation[];
-   constructor(private apiService: ApiService, private authService: AuthService) {}
+
+   constructor() {}
 
    ngOnInit(): void {
-      this.getSingerEventLocations('asc', 'name', 0, 1000, '').subscribe(
-         res => {
-            this.availableLocations = res.items as SingerEventLocation[];
-         }
-      );
-      this.authService.isAuthenticated$.subscribe((res) => {
-         this.isAuthenticated = res;
-      });
 
-      this.authService.isAuthenticated();
    }
 
-   getSingerEventLocations(
-      sortDirection?: string,
-      sortColumn?: string,
-      pageIndex?: number,
-      pageSize?: number,
-      filter?: string
-   ): Observable<PaginationDTO> {
-      const searchParams = new HttpParams()
-         .set('sortDirection', sortDirection)
-         .set('sortColumn', sortColumn)
-         .set('pageIndex', pageIndex.toString())
-         .set('pageSize', pageSize.toString())
-         .set('filter', filter);
-      return this.apiService
-         .get('api/eventlocation', searchParams)
-         .pipe(map(res => res));
-   }
-
-   getEvents(searchEventData: SearchEventData): Observable<EventDescription[]> {
-      const searchParams = <SearchEventDTO>{
-         startDate: searchEventData.startDateTime,
-         endDate: searchEventData.endDateTime,
-         locationId: searchEventData.locationId,
-      };
-      return this.apiService
-         .post('api/event/search', searchParams)
-         .pipe(map(res => res));
-   }
-
-   onSearchEvent(searchEventData: SearchEventData) {
-      this.getEvents(searchEventData).subscribe(
-         res =>
-            (this.events = res.map(
-               r =>
-                  new EventDescription(
-                     r.id,
-                     r.title,
-                     r.description,
-                     r.ageGroups,
-                     r.startDateTime,
-                     r.endDateTime
-                  )
-            ))
-      );
-   }
 }
