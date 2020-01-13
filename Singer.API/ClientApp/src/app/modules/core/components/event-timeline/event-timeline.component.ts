bb8f6b8d9a208c2686b3658065a86cf4f27403a4
Joren Thijs <joren.thijs@hotmail.com>
2020-01-13 14:41:49 +0100

    added new timeline color variables added new CSS classes added new test data

--- Singer.API/ClientApp/src/app/modules/core/components/event-timeline/event-timeline.component.ts
+++ Singer.API/ClientApp/src/app/modules/core/components/event-timeline/event-timeline.component.ts
@@ -5,17 +5,14 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './event-timeline.component.html',
   styleUrls: ['./event-timeline.component.css']
 })
-export class EventTimelineComponent implements OnInit {
+export class EventTimelineComponent {
 
+   @Input() displayEventOnly: boolean;
+   @Input() isAnimated: boolean;
    @Input() startRegistrationDate: Date;
    @Input() endRegistrationDate: Date;
    @Input() finalCancelationDate: Date;
    @Input() startEventDate: Date;
    @Input() endEventDate: Date;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
