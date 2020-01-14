a60047e2b350943a4efe63638c9285dd7c517937
Joren Thijs <joren.thijs@hotmail.com>
2020-01-14 01:54:01 +0100

    Removed TestData from admin menu component

--- Singer.API/ClientApp/src/app/modules/dashboard/components/admin-menu/admin-menu.component.ts
+++ Singer.API/ClientApp/src/app/modules/dashboard/components/admin-menu/admin-menu.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SingerEvent } from 'src/app/modules/core/models/singerevent.model';
 
 @Component({
    selector: 'app-admin-menu',
@@ -8,39 +7,4 @@ import { SingerEvent } from 'src/app/modules/core/models/singerevent.model';
 })
 export class AdminMenuComponent {
 
-   startRegistrationDate: Date;
-   endRegistrationDate: Date;
-   finalCancelationDate: Date;
-   startEventDate: Date;
-   endEventDate: Date;
-   constructor() {}
-
-   buttonClick() {
-      if(this.startRegistrationDate == null) {
-         this.startRegistrationDate = new Date('11/01/2020');
-         return;
-      }
-
-      if(this.endRegistrationDate == null) {
-         this.endRegistrationDate = new Date('11/03/2020');
-         return;
-      }
-
-      if(this.finalCancelationDate == null) {
-         this.finalCancelationDate = new Date('11/05/2020');
-         return;
-      }
-
-      if(this.startEventDate == null) {
-         this.startEventDate = new Date('11/07/2020');
-         return;
-      }
-
-      if(this.endEventDate == null) {
-         this.endEventDate = new Date('11/08/2020');
-         return;
-      }
-
-
-   }
 }
