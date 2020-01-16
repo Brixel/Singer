df1ff77a32e4db31e639a77ac7ee3c6c77a15f06
wim van laer <van.laer.wim@gmail.com>
2020-01-16 20:48:08 +0100

    data-seeder: added DummyDataSeeder project

---
+++ Singer.DummyDataSeeder/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Singer.DummyDataSeeder
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
