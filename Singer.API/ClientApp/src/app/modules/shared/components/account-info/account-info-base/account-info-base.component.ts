d0e8d4c7df51cfebf5b91715da04d6fcd521c4f8
Joren Thijs <joren.thijs@hotmail.com>
2020-01-26 15:35:23 +0100

    feat(user-profile-page) Added routerlinks and responsivenes

--- Singer.API/ClientApp/src/app/modules/shared/components/account-info/account-info-base/account-info-base.component.ts
+++ Singer.API/ClientApp/src/app/modules/shared/components/account-info/account-info-base/account-info-base.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
    selector: 'app-account-info-base',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./account-info-base.component.css'],
 })
 export class AccountInfoBaseComponent implements OnInit {
-   constructor() {}
+
+   navbarLinks: NavbarLinks[] = [
+      {
+         displayIcon: 'assignment_ind',
+         displayName: 'Mijn persoonlijke gegevens',
+         url: '/dashboard/account/account-info',
+      },
+      {
+         displayIcon: 'group',
+         displayName: 'Mijn zorggebruikers',
+         url: '/dashboard/account/zorggebruikers',
+      }
+   ];
+
+   constructor(public router: Router) {}
 
    ngOnInit() {}
+
+   navigate(url: string) :void {
+      this.router.navigateByUrl(url);
+   }
+}
+
+interface NavbarLinks {
+   displayIcon: string,
+   displayName: string,
+   url: string,
 }
