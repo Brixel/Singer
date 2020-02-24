7eef8a5ab5c2a13e4befea92cefb39bf5641dc32
Joren Thijs <41837971+Joren-Thijs@users.noreply.github.com>
2020-02-24 21:31:10 +0100

    feat(calendar-nightcare) improved logic and added extra step
    AB#131

--- Singer.API/ClientApp/src/app/modules/legalguardians/shared/register-care-wizard/register-care-wizard.component.ts
+++ Singer.API/ClientApp/src/app/modules/legalguardians/shared/register-care-wizard/register-care-wizard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AddRegisterCareWizardStep } from './add-register-care-wizard-step';
 import {
    SearchCareUserDialogComponent,
@@ -6,13 +6,14 @@ import {
 } from '../search-care-user-dialog/search-care-user-dialog.component';
 import { MatDialog, MatSnackBar, MatStepper, MatButtonToggleChange } from '@angular/material';
 import { OwlDateTimeInlineComponent } from 'ng-pick-datetime';
+import { Router } from '@angular/router';
 
 @Component({
    selector: 'app-register-care-wizard',
    templateUrl: './register-care-wizard.component.html',
    styleUrls: ['./register-care-wizard.component.css'],
 })
-export class RegisterCareWizardComponent implements OnInit {
+export class RegisterCareWizardComponent {
    wizardSteps: AddRegisterCareWizardStep[] = [
       {
          stepLabel: 'Start',
@@ -42,14 +43,22 @@ export class RegisterCareWizardComponent implements OnInit {
       {
          stepLabel: 'Opvang aanvragen',
          topText: 'Selecteer de opvangdatum',
-         addButtonText: 'Opvangdatum selecteren',
-         middleButtonText: 'Samenvatting',
+         addButtonText: '',
+         middleButtonText: '',
+         backArrow: true,
+         forwardArrow: true,
+      },
+      {
+         stepLabel: 'Overzicht',
+         topText: 'Bekijk hier of alle gegevens kloppen.',
+         addButtonText: '',
+         middleButtonText: 'Aanvraag indienen',
          backArrow: true,
          forwardArrow: false,
       },
       {
          stepLabel: 'Klaar',
-         topText: 'Aanvraag klaar om in te dienen',
+         topText: 'Uw aanvraag is succesvol ontvangen.',
          addButtonText: '',
          middleButtonText: 'Terug naar Dashboard',
          backArrow: false,
@@ -76,9 +85,37 @@ export class RegisterCareWizardComponent implements OnInit {
    };
    selectedCareType: any;
 
-   constructor(public dialog: MatDialog, private _snackBar: MatSnackBar) {}
+   constructor(public dialog: MatDialog, private _snackBar: MatSnackBar, private router: Router) {}
 
-   ngOnInit() {}
+   moveStepperBackward() {
+      this.stepper.previous();
+   }
+
+   moveStepperForward(index: number) {
+      if (index === 0) {
+         this.stepper.next();
+      }
+
+      if (index === 1) {
+         this.validateCareType();
+      }
+
+      if (index === 2) {
+         this.validateCareUsers();
+      }
+
+      if (index === 3) {
+         this.validateDates();
+      }
+
+      if (index === 4) {
+         this.registerCare();
+      }
+
+      if (index === this.wizardSteps.length - 1) {
+         this.router.navigateByUrl('/dashboard');
+      }
+   }
 
    handleAddButtonClick() {
       this.addCareUser();
@@ -106,32 +143,17 @@ export class RegisterCareWizardComponent implements OnInit {
       this.careUsers = this.careUsers.filter(x => x !== relatedUser);
    }
 
-   handleMiddleButtonClick(selectedIndex: number) {
-      if (selectedIndex === 0) {
-         this.stepper.next();
-      }
-      if (selectedIndex === 1) {
-         this.validateCareType();
-      }
-
-      if (selectedIndex === 2) {
-         this.validateCareUsers();
-      }
-      if (selectedIndex === 3) {
-         this.validateDates();
-      }
-   }
    validateCareType() {
       console.log(this.dayCareType);
       if (this.dayCareType !== undefined) {
          this.selectedCareType = this.dayCareType;
-
          this.stepper.next();
       } else {
          this._snackBar.open('Selecteer een opvang type', 'OK');
          return;
       }
    }
+
    validateCareUsers() {
       console.log(this.careUsers);
       if (this.careUsers.length === 0) {
@@ -141,6 +163,7 @@ export class RegisterCareWizardComponent implements OnInit {
 
       this.stepper.next();
    }
+
    validateDates() {
       const selectedDateTimeValues = this.datetimepicker.selecteds;
       if (selectedDateTimeValues.length !== 2) {
@@ -152,13 +175,8 @@ export class RegisterCareWizardComponent implements OnInit {
          this.stepper.next();
       }
    }
-   moveStepperBackward() {
-      this.stepper.previous();
-   }
 
-   moveStepperForward() {
-      this.stepper.next();
-   }
+   registerCare() {}
 }
 
 export enum EventRegistrationTypes {
